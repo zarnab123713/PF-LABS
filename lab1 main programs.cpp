93d1bdf41c4119cd697744aab514db363c5a38bc
zarnab123713 <eyesangel787@gmail.com>
2025-04-23 08:18:53 -0700

    Add files via upload

---
+++ lab1 main programs.cpp
@@ -0,0 +1,124 @@
+////------------------------------------------------------------------------------------------------------------------------------------------------------------
+                                             // Task 1: Student Record Management System (Function Inside Class)
+//#include<iostream>
+//#include"Student.h"
+//using namespace std;
+//int main(){
+//	Student std;
+//	std.getData();      
+//	std.displayData();
+//	return 0;
+//}
+//---------------------------------------------------------------------------------------------------------------------------------------------------------
+//                                      //Task 2: Rectangle(Function Outside Class)
+//#include<iostream>
+//#include"Rectangle.h"
+//int main(){
+//	Rectangle rect;
+//  int l,w;
+//	cout<<"Enter length: ";
+// 	cin>>l;
+// 	cout<<"Enter width: ";
+// 	cin>>w;
+//	rect.setDimension(l,w);
+//	cout<<"Area: "<<rect.area()<<endl;;
+// 	cout<<"Perimeter of Rectangle: "<<rect.perimeter();
+//	return 0;
+//}
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------
+                                                             //Task 3: Voting Eligibility
+//#include<iostream>
+//#include"Voting.h"
+//using namespace std; 
+//int main(){
+//	Voter person;
+//	string votername;
+//	int voterage;
+//	cout<<"Enter voter's name: ";
+//	getline(cin, votername);
+//	cout<<"Enter age: ";
+//	cin>>voterage;
+//    person.setData(votername, voterage);
+//    if(person.isEligible()){
+//    	cout<<"\n"<<votername<<" is eligible to vote."<<endl;
+//	}
+//	else{
+//		cout<<"\n"<<votername<<" is not eligible to vote."<<endl;
+//	}
+//	return 0;
+//}
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
+                                                      //Task 4: Temperature Conversion (Celsius to Fahernheit)
+//#include<iostream>
+//#include"Temperature.h"
+//int main(){
+//	Temperature temp;
+//	cout<<"Enter temperature in Celsius: ";
+//	cin>>temp.celsius;
+//	temp.display();
+//	return 0;
+//}
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------
+                                                          //Task 5: Simple Calculator
+//#include<iostream>
+//#include"Calculator.h"
+//using namespace std;
+//int main(){
+//Calculator cal;
+//float a ,b;
+//int choice;
+//cout<<"Enter first no: ";
+//cin>>a;
+//cout<<"Enter second no: ";
+//cin>>b;
+//cal.setvalues(a,b);
+//cout<<"Choose operation:\n";
+//cout<<"1.Add\n2.Subtract\n3.Multiply\n4.Divide\n";
+//cout<<"Enter your choice(1-4): ";
+//cin>>choice;
+//cout<<"\n RESULT: ";
+//switch (choice){
+//	case 1:
+//		cout<<cal.add()<<endl;
+//		break;
+//	case 2:
+//		cout<<cal.subtract()<<endl;
+//		break;
+//	case 3:
+//		cout<<cal.multiply()<<endl;
+//		break;
+//	case 4:
+//		cout<<cal.divide()<<endl;
+//		break;
+//	default:
+//		cout<<"Invalid choice!"<<endl;
+//    }
+//return 0;
+//}
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------
+                                              //Task 6: Encapsulation Product Class
+//#include<iostream>
+//#include"Product.h"
+//using namespace std;
+//int main(){
+//	Product prod;
+//	string name;
+//	double price;
+//	int quantity;
+//	cout<<"Enter product name: ";
+//	getline(cin,name);
+//	prod.setName(name);
+//	cout<<"Enter price: ";
+//	cin>>price;
+//	prod.setPrice(price);
+//	cout<<"Enter quantity: ";
+//	cin>>quantity;
+//	prod.setQuantity(quantity);
+//     	//Displaying information
+//	cout<<"\nProduct Information  "<<endl;
+//	cout<<"Name: "<<prod.getName()<<endl;
+//	cout<<"Price: "<<prod.getPrice()<<endl;
+//	cout<<"Quantity: "<<prod.getQuantity()<<endl;
+//	return 0;
+//}
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------
