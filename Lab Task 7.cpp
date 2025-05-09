13344e8e779421ca736845e9cc20b777b9b3982a
zarnab123713 <eyesangel787@gmail.com>
2025-05-09 01:18:27 -0700

    Add files via upload

---
+++ Lab Task 7.cpp
@@ -0,0 +1,197 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+                                 	//PROGRAM 1:
+//	int n;
+//	cout<<"Enter a number:";
+//	cin>>n;
+//	if(n>0)
+//	{
+//	cout<<"Number is positive.";
+//    }
+//	else if(n<0)
+//	{
+//	cout<<"Number is negative.";
+//    }
+//	else
+//	{
+//	cout<<"Number is zero.";
+//    }
+
+
+                             //PROGRAM 2:
+
+//     int temp;
+//     cout<<"Enter a temperature:";
+//     cin>>temp;
+//     if(temp>=40)
+//     cout<<"Extremely hot weather.Stay indoor.";
+//     else if(temp>=30&&temp<=39)
+//     cout<<"Drink plenty of water and stay hydarated.";
+//     else if(temp>=20&&temp<=29)
+//     cout<<"Weather is pleasent and suitable for outdoor activities.";
+//     else
+//     cout<<"Wear worm clothes to stay comfortable in the cold weather.";
+     
+                     
+					    //PROGRAM 3: 
+     
+
+//     string CORRECT_USERNAME="admin";
+//      string CORRECT_PASSWORD="12345";
+//      string username,password;
+//     cout<<"Enter a username:";
+//     cin>>username;
+//     cout<<"Enter a password:";
+//     cin>>password;
+//     if((username==CORRECT_USERNAME)&&(password==CORRECT_PASSWORD))
+//     cout<<"Access Granted.";
+//     else if((username==CORRECT_USERNAME)&&(password!=CORRCET_PASSWORD))
+//    cout<<"Wrong password.";
+//     else
+//     cout<<"User not found.";
+// 
+ 
+                       //PROGRAM 4:
+//     int age;
+//	 double income;
+//	 cout<<"Enter your age:";
+//	 cin>>age;
+//	 cout<<"Enter your annual income:";
+//	 cin>>income;
+//	 if(age>=18)
+//	{
+//	    if(income>=30000)
+//	    {
+//	    	cout<<"Congratulation!You are eligibile for a loan.";
+//		}
+//		else
+//		{
+//			cout<<"You meet the age requirement but are not eligible for a loan because your income is below $30,000.";
+//	    }
+//	}
+//	    else
+//	    {
+//	    	cout<<"You are not eligible for a loan because you are under 18 years of age.";
+//		}
+
+                           //PROGRAM 05
+	
+//	int mark1,mark2,mark3;
+//	char grade1,grade2,grade3;
+//	float totalmark,percentage;
+//	cout<<"\n Enter the mark of subject1:";
+//	cin>>mark1;
+//	cout<<"\nEnter the mark of sybject2:";
+//	cin>>mark2;
+//	cout<<"\nEnter the mark of subject3:";
+//	cin>>mark3;
+//	totalmark=mark1+mark2+mark3;
+//	cout<<"\nObtained marks ="<<totalmark;
+//	percentage=(totalmark/300)*100;
+//	cout<<"\n Percentage="<<percentage;
+//	if(mark1>=90){
+//		grade1='A';
+//		cout<<"\nThe grade in subject1 is:"<<grade1;
+//	}
+//	else if(mark1>=80&&mark1<90){
+//		grade1='B';
+//		cout<<"\nThe grade in subject1 is:"<<grade1;
+//	}
+//	else if(mark1>=70&&mark1<80){
+//	grade1='C';
+//	cout<<"\nThe grade in subject1 is:"<<grade1;	
+//	}
+//	else if(mark1>=60&&mark1<70){
+//		grade1='D';
+//		cout<<"\nThe grade in subject1 is:"<<grade1;
+//	}
+//	else
+//	{
+//		grade1='F';
+//		cout<<"\nThe grade in subject1 is:"<<grade1;
+//	}
+//	if(mark2>=90){
+//		grade2='A';
+//		cout<<"\nThe grade in subject2 is:"<<grade2;
+//	}
+//	else if(mark2>=80&&mark2<90){
+//		grade2='B';
+//		cout<<"\nThe grade in subject2 is:"<<grade2;
+//	}
+//	else if(mark2>=70&&mark2<80){
+//		grade2='C';
+//		cout<<"\nThe grade in subject2 is:"<<grade2;
+//	}
+//	else if(mark2>=60&&mark2<70){
+//		grade2='D';
+//		cout<<"\nThe grade in subject2 is:"<<grade2;
+//	}
+//	else{
+//		grade2='F';
+//		cout<<"\nThe grade in subject2 is:"<<grade2;
+//	}
+//	if(mark3>=90){
+//		grade3='A';
+//		cout<<"\nThe grade in subject3 is:"<<grade3;
+//	}
+//	else if(mark3>=80&&mark3<90){
+//		grade3='B';
+//		cout<<"\nThe grade in subject3 is:"<<grade3;
+//	}
+//  else if(mark3>=70&&mark3<80){
+//  	grade3='C';
+//  	cout<<"\n The grade in subject3 is :"<<grade3;
+//  }
+//  else if(mark3>=60&&mark3<70){
+//  	grade3='D';
+//  	cout<<"\n The grade in subject3 is:"<<grade3;
+//  }
+//  else{
+//  	grade3='F';
+//  	cout<<"\nThe grade in subject3 is:"<<grade3;
+//  }
+//  if(grade1=='F'||grade2=='F'||grade3=='F'){
+//  	cout<<"\n\n Failed.";
+//  }
+//  else if(grade1=='A'&&grade2=='A'&&grade3=='A'&&totalmark>=270){
+//  	cout<<"\n\n The student is eligible for merit scholarship.";
+//  }
+//  else if(grade1=='B'&&grade2=='B'&&grade3=='B'&&totalmark>=240){
+//  	cout<<"\n\n The student is eligible for regular scholarship.";
+//  }
+//  else{
+//  	cout<<"\n\n Not eligible for scholarship.";
+//  }
+
+                         //PROGRAM 06
+                         
+//   string username,password,role;
+//   const string ADMIN_USERNAME="admin";
+//   const string ADMIN_PASSWORD="12345";
+//   cout<<"\n Enter username:";
+//   cin>>username;
+//   cout<<"\n Enter password:";
+//   cin>>password;
+//   if(username==ADMIN_USERNAME&&password==ADMIN_PASSWORD){
+//    cout<<"\nAuthentication Successful";
+//    cout<<"\n\nEnter role(Admin,Guest,or other):";
+//    cin>>role;
+//          if(role=="Admin"){
+//          	cout<<"Full Access"<<endl;
+//		  }
+//		  else if(role=="Guest"){
+//		  	cout<<"Limited Access"<<endl;
+//		  }
+//		  else{
+//		  	cout<<"No Access"<<endl;
+//		  }
+//    }
+//		else{
+//			cout<<"Authentication Failed.Access Denied."<<endl;
+//		}  
+//                        
+return 0;
+}
